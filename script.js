c40ff0c2f55c4418b9a26c482c2269c26043564c
Juggernaut <oluwapelumifaruq@gmail.com>
2024-02-04 18:34:49 +0100

    Simplify the top nav event listener for mobile view

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ let menuCloseBtn = document.getElementById("menuCloseBtn");
 let navUl = document.getElementById("navUl");
 //$Ppmikey12$;
 
-menuBtn.addEventListener("click", function (params) {
+function navDisplay(params) {
   navUl.classList.toggle("showNav");
   menuBtn.classList.toggle("hide");
   menuCloseBtn.classList.toggle("showNav");
-});
+}
 
-menuCloseBtn.addEventListener("click", function (params) {
-  navUl.classList.toggle("showNav");
-  menuBtn.classList.toggle("hide");
-  menuCloseBtn.classList.toggle("showNav");
-});
+menuBtn.addEventListener("click", navDisplay);
+
+menuCloseBtn.addEventListener("click", navDisplay);
